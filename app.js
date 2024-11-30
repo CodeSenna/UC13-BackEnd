8892ff6e877ee027ede9d42377a4d623a5268ace
CodeSenna <matheus.hssousa@outlook.com>
2024-11-29 22:08:41 -0300

    FOI FEITO GET E POST

--- app.js
+++ app.js
@@ -5,10 +5,35 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/',(requisicao, resposta) => { 
+app.get('/', (requisicao, resposta) => { 
     resposta.status(200).send(carros2024);
 });
 
+app.get('/:sigla',(requisicao, resposta) => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); //Obtendo a sigla.
+    const carro = carros2024.find((infoCarro) => infoCarro.sigla === siglaInformada); //Buscando a sigla.
+    
+    if (!carro) {
+        //Vazio Not.
+        resposta
+        .status(404)
+        .send(
+            'Não existe um carro com a sigla informada' //Mensagem de erro.
+        );
+    return;
+    }
+    resposta.status(200).send(carro); //Se encontrado a sigla retorna a resposta correta.
+});
+
+app.post('/', (req, res ) => {
+    const novoCarro = req.body; //Obtém o corpo enviado para incluir um carro.
+    carros2024.push(novoCarro); //Adiciona o novo carro a array.
+    res.status(200).send(novoCarro); //Retorna o carro add com o status 200(OK).
+});
+
+
 // Inicia o servidor na porta 3000:
-// http://localhost:3000/
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+
+// http://localhost:3000/
+// node app.js
--- tabelacarros.js
+++ tabelacarros.js
@@ -17,6 +17,33 @@ const carros2024 = [
         aceleracao: 2.8,
         preco: 330000,
     },
+    {
+        nome: 'BMW',
+        sigla: 'BMW',
+        velocidade: 200,
+        potencia: 500,
+        consumo: 7.5,
+        aceleracao: 4.0,
+        preco: 120000,
+    },
+    {
+        nome: 'Audi',
+        sigla: 'AUD',
+        velocidade: 290,
+        potencia: 650,
+        consumo: 7.0,
+        aceleracao: 3.6,
+        preco: 115000,
+    },
+    {
+        nome: 'Ford',
+        sigla: 'FOR',
+        velocidade: 230,
+        potencia: 250,
+        consumo: 11.0,
+        aceleracao: 6.0,
+        preco: 40000,
+    },  
 ];
 
 export default carros2024;
